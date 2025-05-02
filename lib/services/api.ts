742929ac33b34d5d8f4519fc9a38b2ba48318728
viettt76 <viettt76@gmail.com>
2025-05-02 20:58:52 +0700

    [FIX] axios timeout

--- lib/services/api.ts
+++ lib/services/api.ts
@@ -3,7 +3,6 @@ import axios from 'axios';
 const instance = axios.create({
     baseURL: process.env.NEXT_PUBLIC_API_URL,
     withCredentials: true,
-    timeout: 5000,
 });
 
 instance.interceptors.request.use(
